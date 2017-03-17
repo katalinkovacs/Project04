4bbab8c2ce9dba738cd6a06b77bba033bc85cf25
katalinkovacs <katalin.kovacs@marlo.com.au>
2017-03-17 11:38:15 +1100

    blueprints created

--- PersonPlatform/src/main/java/routes/RoutePersonPlatform.java
+++ PersonPlatform/src/main/java/routes/RoutePersonPlatform.java
@@ -6,9 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class Route1 extends RouteBuilder{
+public class RoutePersonPlatform extends RouteBuilder{
 
-    private static final Logger logger = LoggerFactory.getLogger(Route1.class);
+    private static final Logger logger = LoggerFactory.getLogger(RoutePersonPlatform.class);
 
     @Override
     public void configure() throws Exception {
